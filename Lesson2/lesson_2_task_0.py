7d9e775a28965a5b98ea2a91f489148e21d6a18a
emma-forshtreter <emma.forshtreter@gmail.com>
2024-07-23 01:51:29 +0500

    HW2

--- Lesson2/lesson_2_task_0.py
+++ Lesson2/lesson_2_task_0.py
@@ -1,6 +1,12 @@
-my_age = input("Сколько Вам полных лет? ")
-print("Ваш возраст: " + my_age)
+#my_age = input("Сколько Вам полных лет? ")
+#print("Ваш возраст: " + my_age)
 
-my_age = int("62") + 1
-print("Ваш возраст через год: ")
-print(my_age)
+#my_age = int("62") + 1
+#print("Ваш возраст через год: ")
+#print(my_age)
+
+my_age = int(input("Сколько Вам полных лет? "))
+print(f'Ваш возраст: {my_age}')
+
+my_age = my_age + 1
+print(f'Ваш возраст через год: {my_age}')
--- Lesson2/lesson_2_task_3.py
+++ Lesson2/lesson_2_task_3.py
@@ -1,9 +1,16 @@
-print('Введите сторону квадрата: ')
-side = input()
-side = int(side)
-def square(print_square):
-    print('Площадь квадрата со стороной ', end="")
-    print(side, end="")
-    print(' равна ', end="")
-    print(print_square, end="")
-square(side * side)
+#print('Введите сторону квадрата: ')
+#side = input()
+#side = int(side)
+#def square(print_square):
+#    print('Площадь квадрата со стороной ', end="")
+#    print(side, end="")
+#    print(' равна ', end="")
+#    print(print_square, end="")
+#square(side * side)
+
+def square(side):
+    return side*side
+
+import math
+side = int(input("Введите сторону квадрата: "))
+print(f'Площадь квадрата: {square(side)}')
